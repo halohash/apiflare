cf629bf0a93b57fab3da3d68cbc9288c6deee1e6
giggityfiles <giggityfiles@gmail.com>
2026-04-20 21:42:19 -0400

    ok

---
+++ functions/randompics.js
@@ -0,0 +1,33 @@
+export async function onRequest(context) {
+  // List of image URLs
+  const images = [
+    "https://picsum.photos/800/600",
+    "https://placekitten.com/800/600",
+    "https://file.garden/aUYIWVAKvQxCBY-_/database/images/DirecTV_5_LNB_Slimline_2012_06_08.jpg",
+    "https://file.garden/aUYIWVAKvQxCBY-_/database/images/puckett.png",
+    "https://file.garden/aUYIWVAKvQxCBY-_/database/images/cmd-hue.github.io_woketv_.png"
+  ];
+
+  // Pick a random image
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+try {
+    const response = await fetch(randomImage);
+
+    // If the fetch worked but returned an error status
+    if (!response.ok) {
+      return new Response("Failed to fetch image", { status: 400 });
+    }
+
+    return new Response(response.body, {
+      headers: {
+        "Content-Type": response.headers.get("Content-Type") || "image/jpeg",
+        "Cache-Control": "no-store"
+      }
+    });
+
+  } catch (err) {
+    // If fetch throws (network failure, DNS, etc.)
+    return new Response("<h1>IMAGE GRAB WASN'T OK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>", { status: 400 });
+  }
+  };
---
+++ functions/squig.js
@@ -0,0 +1,26 @@
+export async function onRequest(context) {
+  const targetUrl = "https://www.vidtape.lol/Active_Sharing"; // change this to your URL
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: "GET",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 10 Build/LMY48I) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.84 Safari/537.36"
+      }
+    });
+
+    // If the fetch fails (non-2xx status), return 400
+    if (!response.ok) {
+      return new Response("Resource Not Ok", { status: 400 });
+    }
+
+    // Return the fetched content
+    return new Response(response.body, {
+      status: response.status,
+      headers: response.headers
+    });
+
+  } catch (err) {
+    return new Response("squidwardinsidehouse 400 error jr.", { status: 400 });
+  }
+}
